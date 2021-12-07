e04c8a225fda2509f0dc735adb76931a3c5a631b
Mauro Balbi <maurobalbi@bluewin.ch>
2021-12-07 11:59:08 +0100

    6a

--- 6a.hs
+++ 6a.hs
@@ -7,5 +7,16 @@ import qualified Prelude as P
 main :: IO ()
 main = interact' $ f . map (read :: String -> Int) . splitOn ","
 
-f :: [Int] -> [Int]
-f = id
+f :: [Int] -> Int
+f = length . (!! 80) . iterate progressDay
+
+
+progressDay :: [Int] -> [Int]
+progressDay xs = do
+  x <- xs 
+  if x == 0
+    then
+      [6,8]
+    else 
+      pure $ x - 1
+
