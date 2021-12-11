27869fb35a76107ffab030618ea595d71594e953
Mauro Balbi <maurobalbi@bluewin.ch>
2021-12-11 22:52:09 +0100

    11a outline working

--- 11a.hs
+++ 11a.hs
@@ -4,6 +4,7 @@
 
 import AOC
 import qualified Data.Map as M
+import Debug.Trace
 
 main :: IO ()
 main = interact $ run . map (read @Int)
@@ -16,13 +17,13 @@ run xs = execState (f xs) 0
 f :: MonadState Int m => [Int] -> m [Int]
 f xs = do
   if 9 `elem` xs -- have to iterate over all 9s e.g. forM
-    then f ((+ 1) <$> xs)
+    then foldM (\acc x -> f ((\x -> if x == 0 then 0 else x + 1) <$> acc)) xs flash 
     else do 
       modify (+ updates)
       pure $ reset <$> xs
   where
-    flash = filter (> 8) xs
-    updates = length flash
+    flash = filter (== 9) xs
+    updates = length $ filter (> 8) xs
     reset x
       | x > 8 = 0
       | otherwise = x
